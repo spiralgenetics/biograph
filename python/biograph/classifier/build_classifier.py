e2a65ac0d1cf20f1654e433ac4a3a62288b6dbf3
shkoo <nils@shkoo.com>
2021-07-22 16:28:44 -0700

    Feature/continuous integration (#8)
    * Add bazel test github action
    
    * Install python 3.6 before running tests
    
    * Need to install python as root
    
    * Make docker image without reference included, have github CI use it
    
    * Only compile for a single python version when running tests
    
    * Call github CI "github-ci" in all the places for consistency
    
    * Keep going on test failures so we can get  full summary
    
    * Archive test outputs, cache bazel build artifacts
    
    * fix yaml syntax
    
    * Fix pylint errors
    
    * Use 4 GB mem to test import formats
    
    * Cache apt packages so we don't have to keep downloading them
    
    * Test github CI caching
    
    * Remove wildcards from test archiving
    
    Co-authored-by: Nils McCarthy <nils.mccarthy@gmail.com>
    Co-authored-by: nils <nils@spiralgenetics.com>

--- python/biograph/classifier/build_classifier.py
+++ python/biograph/classifier/build_classifier.py
@@ -7,8 +7,6 @@ import sys
 import argparse
 
 import joblib
-import pandas as pd
-import numpy as np
 from sklearn.ensemble import RandomForestClassifier
 #import biograph.tools.log as log
 
@@ -32,40 +30,40 @@ class build_classifier:
     def __init__(self):
         pass
 
-    def convert_object2integer(self,data):
+    def convert_object2integer(self, data):
         """
         Converts object type columns from grm.jl to numeric
         """
         dct = {'-':-1, '+':1}
-        cols = ['rup_max_strand','rup_min_strand', 'rdn_max_strand', 'rdn_min_strand', 'aup_max_strand',
-       'aup_min_strand', 'adn_max_strand', 'adn_min_strand']
-        for col in cols : 
+        cols = ['rup_max_strand', 'rup_min_strand', 'rdn_max_strand', 'rdn_min_strand', 'aup_max_strand',
+                'aup_min_strand', 'adn_max_strand', 'adn_min_strand']
+        for col in cols:
             data[col] = data[col].map(dct).fillna(0)
         return(data)
-    
+
     def norm_rl(self, df):
         """
         Normalizes read length dependent features
         """
         rl_feat = ["US_r", "US_a", "DS_a", "DS_r", "UXO_r", "UXO_a", "DXO_r", "DXO_a", "UMO_r", "UMO_a", "DMO_r", "DMO_a", "MO_r", "MO_a", "XO_r", "XO_a"]
         rl = df['MO_r'].max()
-        df[rl_feat] = df[rl_feat]/rl
+        df[rl_feat] = df[rl_feat] / rl
         return df
-    
+
     def prepare_input(self, ret, sv_only=False):
         """
         Prepares data for training
-        """        
-        type_d = {'DEL':-1, 'INS':1,'SUBSDEL':-1,'SUBSINS':1 }
+        """
+        type_d = {'DEL': -1, 'INS': 1, 'SUBSDEL': -1, 'SUBSINS': 1}
         ret['VARTYPE'] = ret['var_type'].map(type_d).fillna(0)
-        ret['VAR_LEN_TYPE'] = ret['VARLEN']*ret['VARTYPE']
-        ret = ret[ret['RC'].notna()] 
+        ret['VAR_LEN_TYPE'] = ret['VARLEN'] * ret['VARTYPE']
+        ret = ret[ret['RC'].notna()]
         if sv_only:
             ret = ret[abs(ret["VARLEN"]) >= 50]
         else:
             ret = ret[abs(ret["VARLEN"]) < 50]
-        ret = convert_object2integer(ret)
-        ret = norm_rl(ret)
+        ret = self.convert_object2integer(ret)
+        ret = self.norm_rl(ret)
         return(ret)
 
     def build_model(self, train, features, target, clf):
@@ -96,16 +94,16 @@ def main(args):
     train_fld = joblib.load(args.train)
     train = train_fld['data']
     features = train_fld['features']
-    
+
     bc = build_classifier()
 
     train = bc.prepare_input(train, args.svonly)
 
 #    log.info("Building")
-  
+
     target = 'state_TP'
     clf = RandomForestClassifier(n_jobs=-1, random_state=0, n_estimators=500,
-                                           bootstrap=True, max_features="sqrt", max_depth=30)
+                                 bootstrap=True, max_features="sqrt", max_depth=30)
     model = bc.build_model(train, features, target, clf)
 
 #   log.info("Writing")
